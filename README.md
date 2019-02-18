cff6a5e67fdf03652aafbf2d4e2c93fdb15be30b
rsa153 <r-saber@nlaw.northwestern.edu>
2019-02-17 23:34:46 -0600

    Added comments and links in readme file

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# liri-node-app
+# **LIRI Application**
+
+## **What the project does**
+LIRI uses the axios package to grab data from the Bands in Town, Spotify and OMDB APIs and returns information to the user in the terminal.
+
+**How to Search these APIs**
+ You can use these commands to search these APIs in two ways:
+    1. Using your command line by entering the following commands: concert-this, spotify-this-song, movie-this,
+    2. Using a linked random.txt file where you can also input your command
+
+**What Each Command Does**
+*concert-this*
+    Input the following in your command line: `node liri.js concert-this <artist/band name here>`. LIRI will return the following information from the Bands in Town API:
+        * Venue Name
+        * Venue Location
+        * Date of the Event (MM/DD/YYYY)
+
+*spotify-this-song*
+    Input the following in your command line: `node liri.js spotify-this-song <song name here>`. LIRI will return the following information from the Spotify API:
+        * Artist(s)
+        * Song's name
+        * Album Name
+        * Preview link of the song
+        * Note: *If no song is provided then you will be presented information for "The Sign" by Ace of Base.*
+
+*movie-this*
+    Input the following in your command line: `node liri.js movie-this <movie name here>`. LIRI will return the following information from the OMDB API:
+        * Movie Title
+        * Movie Year
+        * IMDB Rating
+        * Rotten Tomatoes Rating
+        * Country where the movie was produced.
+        * Language of the movie.
+        * Plot of the movie.
+        * Actors in the movie.
+        * Note: *If no movie is provided then you will be presented information for the move "Mr. Nobody".*
+
+*do-what-it-says*
+    Input the following in your command line: `node liri.js do-what-it-says`. LIRI will access the linked random.txt file and read the "concert-this", "spotify-this-song", or "movie-this" command and the artist/song/movie name. LIRI will run the command referenced in the random.txt file. As an example, I've included the spotify-this-song command and the song "I Want it That Way" in the random.txt file.
+
+## **To get started with LIRI**
+ In order to get started with this application, you must supply your own `.env` file and you most install node and all applicable node packages (axios, moment, dotenv) and APIs (Spotify)
+ 
+ If someone wanted to clone your app from github and run it themselves, they would need to supply their own  for it to work.
+
+## **For more Information**
+In order to access this application from my portfolio, please visit: https://rsa153.github.io/bootstrap-portfolio/portfolio.html and click on the `LIRI Code App` link.
+In order to see how this application works, please visit: https://drive.google.com/file/d/15EFxMRrjNBzMLJdBFzatAakPIg3IEoj4/view
--- liri.js
+++ liri.js
@@ -1,44 +1,108 @@
-require("dotenv").config();
+// fs is a core Node package for reading and writing files
 var fs = require('fs');
+
+//requires .env file
+require("dotenv").config();
+
+//requires keys.js
 var keys = require("./keys.js");
+
+//requires axios for omdb and bandsintown api
 var axios = require("axios");
+
+//requires moment
 var moment = require("moment");
+
+//Import the Spotify API
 var Spotify = require("node-spotify-api");
+
+//Create a Spotify Client
 var spotify = new Spotify(keys.spotify);
+
+//defining command as the user input in process.argv[2]
 var command = process.argv[2];
-var name = process.argv.slice(3).join("");
 
-
+//Switch statement used to select one of the four code blocks to be executed based on user process.argv[2] input.
 switch (command) {
+    
+    // code to be executed if process.argv[2] is equal to concert-this;
     case "concert-this":
-        concertThis(name);
+        
+        //will remove the first 3 items in the "argv" array and join the remaining items with %20
+        var name = process.argv.slice(3).join("%20");
+        
+        //if nothing is inputted after concert-this, a message is displayed to the user.
+        if (!name) {
+            console.log("You must input the name of an artist. Please try again.");
+        }
+        
+        //otherwise, the concertThis function will run
+        else {
+            concertThis(name);
+        }
         break;
 
+    //code to be executed if process.argv[2] is equal to spotify-this-song
     case "spotify-this-song":
-        spotifyThis(name);
+    
+        //will remove the first 3 items in the "argv" array and join the remaining items with "+"
+        var name = process.argv.slice(3).join("+");
+        
+        //if nothing is inputted after concert-this, the user will be presented information for Ace of Base's "The Sign".
+        if (!name) {
+            console.log("You didn't chose a song so we chose one for you. Here is The Sign by Ace of Base." +
+                "\n========================");
+            console.log(spotifyThis("The+Sign+Ace+of+Base"))
+        }
+
+        //otherwise, the spotifyThis function will run
+        else {
+            spotifyThis(name);
+        }
         break;
 
+    //code to be executed if process.argv[2] is equal to movie-this
     case "movie-this":
-        movieThis(name);
+        
+        //will remove the first 3 items in the "argv" array and join the remaining items with "+"
+        var name = process.argv.slice(3).join("+");
+        
+        //if nothing is inputted after concert-this, the user will be presented information about Mr. Nobody.
+        if (!name) {
+            console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>" +
+                "\nIt's on Netflix!" +
+                "\n========================");
+        }
+         
+        //otherwise, the movieThis function will run
+        else {
+            movieThis(name);
+        }
         break;
 
+    //code to be executed if process.argv[2] is equal to do-what-it-says
     case "do-what-it-says":
+        
+         //The doThis function will run
         doThis();
         break;
 
+    //default specifies the code to run if the user does not input process.argv[2]
     default:
-        console.log("Please input: concert-this or spotify-this-song or movie-this or do-what-it-says")
+        console.log("Please input concert-this or spotify-this-song or movie-this or do-what-it-says")
 }
-
+//Concert-this function
 function concertThis(name) {
+
+    //bandsintown url
     var URL = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp"
-    axios.get(URL)
-        .then(function (response) {
-            if (response.data = "[]") {
-                console.log("There are no entries for this artist. Please try another artist")
-            }
-            else {
-                for (var i = 0; i < response.data.length; i++) {
+
+    //Using get method to start an HTTP request
+    axios.get(URL).then(function (response) {
+
+            //try statement allows testing of a block of code for errors. If no errors, occur display 5 results
+            try {
+                for (var i = 0; i < 5; i++) {
                     var concertInfo = [
                         "Venue Name: " + response.data[i].venue.name,
                         "Venue Location: " + response.data[i].venue.city,
@@ -48,42 +112,104 @@ function concertThis(name) {
                     console.log(concertInfo);
                 }
             }
+
+            //Catch statement allows handling of error.
+            catch (err) {
+                console.log("There is an error. Please try again.")
+            }
         })
-    // .catch(function(err){
-    //     console.log(err);
-    // })
 }
-//Search method using soitfy API
+
+//Search method using spotfy API
 function spotifyThis(name) {
-    if (!name){
-        name = "TheSign";
-        console.log(name);
-    }
+
+    //search function that specifies type and the query
     spotify.search({
         type: "track",
         query: name,
     }, function (err, data) {
+
+        //If an error occurs, do this
         if (err) {
-           console.log("Error occured: " + err);
-           return;
+            console.log("Error occured: " + err);
         }
+
+        //otherwise, return 5 search results 
         else {
             for (var i = 0; i < 5; i++) {
                 var songInfo = data.tracks.items[i];
-                console.log(name);
-                console.log(songInfo.name);
-                // var songInfo = [
-                //     "Artist: " + track.artists[0].name,
-                //     "Song Name: " + track.name,
-                //     "Album Name: " + track.album.name,
-                //     "Preview link: " + track.preview_url,
-                //     "========================"
-                // ].join("\n");
-                // console.log(songInfo);
-                }
-                // if (songInfo.preview_url = "null") {
-                //     console.log("We could not find a preview link");
-                // } 
-    }
-    });
+                var songInfo = [
+                    "Artist: " + songInfo.artists[0].name,
+                    "Song Name: " + songInfo.name,
+                    "Album Name: " + songInfo.album.name,
+                    "Preview link: " + songInfo.preview_url,
+                    "========================"
+                ].join("\n");
+                console.log(songInfo);
+            }
+        }
+    })
+}
+
+//Movie-this function
+function movieThis(name) {
+
+    //Omdb url
+    var URL = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy"
+    
+    //Using get method to start an HTTP request
+    axios.get(URL).then(function (response) {
+
+        //try statement allows testing of a block of code for errors. If no errors occur, display movie info
+        try {
+            var movieInfo = [
+                "========================",
+                "Movie Title: " + response.data.Title,
+                "Movie Year: " + response.data.Year,
+                "IMDB Rating: " + response.data.imdbRating,
+                "Rotten Tomatos Rating: " + response.data.tomatoRating,
+                "Country of Production: " + response.data.Country,
+                "Language: " + response.data.Language,
+                "Plot: " + response.data.Plot,
+                "Actors in Movie: " + response.data.Actors,
+                "========================"
+            ].join("\n");
+            console.log(movieInfo);
+        }
+
+        //Catch statement allows handling of error.
+        catch (err) {
+            console.log("There is an error.")
+        }
+    })
+}
+
+//doThis function
+function doThis() {
+
+    //Read random.txt file.
+    fs.readFile("./random.txt", "utf8", function (error, data) {
+        
+        //Split the data separated wiith ","
+        var dataSplit = data.split(",");
+
+        //the first position of the data will be called doThat
+        var doThat = dataSplit[0];
+        
+        //the second position of the data will be called name and the "" will be removed by slicing.
+        var name = dataSplit[1].slice(1, -1);;
+        
+        //if doThat equals concert-this, run the concertThis function
+        if (doThat === "concert-this") {
+            concertThis(name);
+        
+        //if doThat equals spotify-this-song, run the spotifyThis function
+        } else if (doThat === "spotify-this-song") {
+            spotifyThis(name);
+
+        //if doThat equals movie-this, run the movieThis function
+        } else if (doThat === "movie-this") {
+            movieThis(name);
+        }
+    })
 }
